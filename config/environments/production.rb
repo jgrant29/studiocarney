3348317ba741d94c92dd1fc34063fdeba373d0c2
Justin Grant <jmgrant702@gmail.com>
2016-06-15 18:48:59 -0600

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'studiocarneyv2.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'frozen-atoll-64894.herokuapp.com/' }
 end
