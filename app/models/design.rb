ede4281c04183c7a2da4672da9a597966662c625
Justin Grant <jmgrant702@gmail.com>
2016-07-01 15:12:30 -0600

    design sort

--- app/models/design.rb
+++ app/models/design.rb
@@ -2,6 +2,8 @@ class Design < ActiveRecord::Base
   mount_uploader :picture, PictureUploader
   has_and_belongs_to_many :design_image_attachments
 
+  default_scope { order('created_at DESC') }
+
 
   def design_last
     @designs.last 
