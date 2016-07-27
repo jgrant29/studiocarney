0e2d6c1a1dc7870fe77a5e8aa6fbc73deea2c1aa
Justin Grant <jmgrant702@gmail.com>
2016-07-26 21:35:50 -0600

    cloudfront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'www.studiocarney.com' }
-  config.action_controller.asset_host = ENV['CDN']
+  config.action_controller.asset_host = 'd1afdpluwto7bd.cloudfront.net'
 end
